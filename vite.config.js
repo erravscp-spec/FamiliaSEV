bc1839e31886cc546774540cd65f90157301e346
erravscp-spec <erravscp@gmail.com>
2026-06-23 12:58:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // IMPORTANTE: troca 'dashboard-bebe' pelo nome exato do teu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard-bebe/',
+  base: '/FamiliaSEV/',
 })
